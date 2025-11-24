9f71eab6c28a22b16ea5280302849ce3ad26e776
杨振豪 <yangshulin2333@gmail.com>
2025-11-24 17:12:36 +0800

    vault backup: 2025-11-24 17:12:36

---
+++ DevEnvironment/Scripts/set-env.bat
@@ -0,0 +1,5 @@
+@echo off
+set JAVA_HOME=D:\Yanzo\yanzo_note\DevEnvironment\Runtimes\Java\JDK17
+set MAVEN_HOME=D:\Yanzo\yanzo_note\DevEnvironment\Tools\Maven
+set NODE_HOME=D:\Yanzo\yanzo_note\DevEnvironment\Runtimes\NodeJS\Node18
+set PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%NODE_HOME%;%PATH%
